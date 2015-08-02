bbe85f836c6931df90c0798d348856c76556dbae
Luis E. I. Herrera <lherrera@sdf.org>
2015-08-02 23:46:08 +1000

    StripedWords

--- OReilly/StripedWords.py
+++ OReilly/StripedWords.py
@@ -1,29 +1,41 @@
+#!/usr/bin/env python3
+
+import re
+
 VOWELS = "AEIOUY"
 CONSONANTS = "BCDFGHJKLMNPQRSTVWXZ"
 
-
 def checkio(text):
-    text = text.upper().strip('.,').split()
+    # Regular Expression Search. Create a list of words composed
+    # only of (converted) UPPERcase alpha characters.
+    text = re.findall(r"\w+",text.upper())
     count = 0
-    
     for word in text:
-        letter_count = 0
-        for char in word:
-            if char in VOWELS:
-                letter_count += 1
-            elif char in CONSONANTS:
-                letter_count -= 1
-                    
-            if letter_count > 1 or letter_count < -1:
-                letter_count = 0
-                pass
-            else:
-                letter_count = 0
-                letter_count = 0
+        if len(word) < 2:
+            continue
+        else:
+            isVowel = ''
+            isValid = True
+            for char in word:
+                if (char not in VOWELS) and (char not in CONSONANTS):
+                    isValid = False
+                    break
+                elif char in VOWELS:
+                    if isVowel == '' or isVowel == False:
+                        isVowel = True
+                    else:
+                        isValid = False
+                        break
+                elif char in CONSONANTS:
+                    if isVowel == '' or isVowel == True:
+                        isVowel = False
+                    else:
+                        isValid = False
+                        break
+            if isValid:
                 count += 1
-        
-        
     return count
+
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
